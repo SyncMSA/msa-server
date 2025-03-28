cbdd2f0dec30e140fe1b37d70e8c969c90385b80
minnnseokk <124752866+minnnseokk@users.noreply.github.com>
2025-03-28 15:55:23 +0900

    feat(track) : set init file (#11)
    set init file

---
+++ syncfit-track-service/src/test/java/com/syncfittrackservice/SyncfitTrackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syncfittrackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncfitTrackServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
