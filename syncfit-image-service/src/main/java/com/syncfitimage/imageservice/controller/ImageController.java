7568d73194ab834a9f3a0d88acabe23845600b81
xecond <ksw09203@gmail.com>
2025-03-29 01:47:30 +0900

    feat: S3 업로드 및 다운로드 기본 구현

--- syncfit-image-service/src/main/java/com/syncfitimage/imageservice/controller/ImageController.java
+++ syncfit-image-service/src/main/java/com/syncfitimage/imageservice/controller/ImageController.java
@@ -1,6 +1,13 @@
 package com.syncfitimage.imageservice.controller;
 
+import com.syncfitimage.imageservice.service.ImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,17 +15,23 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 @RequestMapping("/image")
 public class ImageController {
-//    private final ImageService imageService;
-//    private final S3Service s3Service;
 
-    @PostMapping
-    public void uploadImage(@RequestPart("wishlistId") String wishlistId,
-                            @RequestPart("files") MultipartFile file) {
-//        imageService.uploadWishlistImage(file, wishlistId);
-//        S3Service.uploadFile(file);
+    Environment env;
+    ImageService imageService;
+
+    @Autowired
+    public ImageController(Environment env, ImageService imageService) {
+        this.env = env;
+        this.imageService = imageService;
     }
 
-    /*
+    @PostMapping
+    public ResponseEntity<String> uploadImage(@RequestPart("wishlistId") String wishlistId,
+                                              @RequestPart("files") MultipartFile file) {
+        return ResponseEntity.ok((imageService.uploadWishlistImage(file, wishlistId)));
+    }
+
+
     @GetMapping
     public ResponseEntity<byte[]> downloadImage(@RequestParam("wishlistId") String wishlistId) {
         byte[] imageData = imageService.downloadImage(wishlistId);
@@ -29,5 +42,5 @@ public class ImageController {
 
         return new ResponseEntity<>(imageData, headers, HttpStatus.OK);
     }
-     */
+
 }
