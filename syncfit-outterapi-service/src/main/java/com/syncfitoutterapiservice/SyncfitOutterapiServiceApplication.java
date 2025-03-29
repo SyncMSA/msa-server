c56ba4ce3eeb1080a22c15a51dcb917e79b84305
minnnseokk <124752866+minnnseokk@users.noreply.github.com>
2025-03-29 17:23:15 +0900

    chore(Eureka) : 내부 서비스 유레카 적용 (#32)
    * chore: outerapi eureka set
    
    * chore: track eureka set
    
    * chore: wishlist eureka set

--- syncfit-outterapi-service/src/main/java/com/syncfitoutterapiservice/SyncfitOutterapiServiceApplication.java
+++ syncfit-outterapi-service/src/main/java/com/syncfitoutterapiservice/SyncfitOutterapiServiceApplication.java
@@ -3,7 +3,7 @@ package com.syncfitoutterapiservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.syncfitcommon", "com.syncfitoutterapiservice"})
+@SpringBootApplication(scanBasePackages = {"com.syncfitcommonjpa", "com.syncfitoutterapiservice"})
 public class SyncfitOutterapiServiceApplication {
 
 	public static void main(String[] args) {
