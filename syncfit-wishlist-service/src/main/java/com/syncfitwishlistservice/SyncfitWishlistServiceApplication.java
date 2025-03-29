c8a7b42d0cb8b39b0c7b935ec52e894b7e47a39b
minnnseokk <msjeong991029@gmail.com>
2025-03-29 17:17:39 +0900

    chore: wishlist eureka set

--- syncfit-wishlist-service/src/main/java/com/syncfitwishlistservice/SyncfitWishlistServiceApplication.java
+++ syncfit-wishlist-service/src/main/java/com/syncfitwishlistservice/SyncfitWishlistServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(scanBasePackages = {"com.syncfitcommon", "com.syncfitwishlistservice"})
+@SpringBootApplication(scanBasePackages = {"com.syncfitcommonjpa", "com.syncfitwishlistservice"})
 @EnableFeignClients(basePackages = {"com.syncfitwishlistservice.client"})
 @EnableDiscoveryClient
 public class SyncfitWishlistServiceApplication {
